e039452e652da1881b3abcec8f7739dce2516a66
michal1803 <michaw49@gmail.com>
2022-12-07 14:50:24 +0100

    Full menu added

--- project.ps1
+++ project.ps1
@@ -17,19 +17,103 @@ function showMainMenu {
     Write-Host "1: Obsługa kont użytkowników"
     Write-Host "2: Obsługa kont grup"
     Write-Host "3: Raporty"
-    Write-Host "Q: Wyjście"
+    Write-Host "Q: Wyjdź"
 }
 
-do
- {
+function showUserMenu {
+    param (
+        [string]$Title = 'Obsługa kont użytkowników'
+    )
+    Clear-Host
+    Write-Host "================ $Title ================"
+    
+    Write-Host "1: Tworzenie konta użytkowanika"
+    Write-Host "2: Tworzenie wielu kont na podstawie pliku csv"
+    Write-Host "3: Wyłączenie konta użytkownika"
+    Write-Host "4: Zmiana hasła konta użytkownika" 
+    Write-Host "B: Cofnij"
+    Write-Host "Q: Wyjdź"
+}
+
+function showGroupMenu {
+    param (
+        [string]$Title = 'Obsługa kont grup'
+    )
+    Clear-Host
+    Write-Host "================ $Title ================"
+    
+    Write-Host "1: Tworzenie nowych grup"
+    Write-Host "2: Dodawania użytkowników do grup"
+    Write-Host "B: Cofnij" 
+    Write-Host "Q: Wyjdź"
+}
+
+function showRaportMenu {
+    param (
+        [string]$Title = 'Raporty'
+    )
+    Clear-Host
+    Write-Host "================ $Title ================"
+    
+    Write-Host "1: Lista grup z członkami"
+    Write-Host "2: Lista wyłączonych kont w domenie"
+    Write-Host "3: Lista szczegółowych informacji o kontach użytkowników" 
+    Write-Host "4: Lista szczegółowych informacji o kontach komputerów w domenie" 
+    Write-Host "5: Lista jednostek organizacyjnych w domenie" 
+    Write-Host "B: Cofnij" 
+    Write-Host "Q: Wyjdź"
+}
+
+function showUserMenuCases {
+    do {
+        showUserMenu
+        $selection = Read-Host "Twój wybór"
+        switch ($selection) {
+            'b' { showMainMenu } 
+            'q' { break outer }
+        }
+        pause
+    }
+    until ($selection -eq 'q')
+    
+}
+
+function showGroupMenuCases {
+    do {
+        showGroupMenu
+        $selection = Read-Host "Twój wybór"
+        switch ($selection) {
+            'b' { showMainMenu } 
+            'q' { break outer }
+        }
+        pause
+    }
+    until ($selection -eq 'q')
+    
+}
+function showRaportMenuCases {
+    do {
+        showRaportMenu
+        $selection = Read-Host "Twój wybór"
+        switch ($selection) {
+            'b' { showMainMenu } 
+            'q' { break outer }
+        }
+        pause
+    }
+    until ($selection -eq 'q')
+    
+}
+
+:outer do {
     showMainMenu
     $selection = Read-Host "Twój wybór"
-    switch ($selection)
-    {
-    '1' {'Wybrałeś opcję: Obsługa kont użytkowników'} 
-    '2' {'Wybrałeś opcję: Obsługa kont grup'}
-    '3' {'Wybrałeś opcję: Raporty'}
+    switch ($selection) {
+        '1' { showUserMenuCases } 
+        '2' { showGroupMenuCases }
+        '3' { showRaportMenuCases }
+        'q' { break outer }
     }
     pause
- }
- until ($selection -eq 'q')
+}
+until ($selection -eq 'q')
