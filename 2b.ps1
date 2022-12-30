b29e6b0cc7776208b97a63ff99936905388958c5
michal1803 <michaw49@gmail.com>
2022-12-30 10:31:35 +0100

    addusertogroup function added

---
+++ 2b.ps1
@@ -0,0 +1,41 @@
+$indexNumber = "18852"
+function addUserToGroup {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$groupName,
+        [Parameter(Mandatory = $true)]
+        [string]$userName
+    )
+
+    # Pobierz obiekt użytkownika z AD
+    $user = Get-ADUser -Identity $userName -ErrorAction SilentlyContinue
+
+    # Pobierz obiekt grupy z AD
+    $group = Get-ADGroup -Identity $groupName -ErrorAction SilentlyContinue
+
+    if(-not $user -or -not $group) {
+        Write-Output "Użytkownik lub grupa nie są poprawne. Wprowadź raz jeszcze."
+        return addUserToGroup
+    }
+
+    # Sprawdź, czy użytkownik jest członkiem grupy
+    if ($user.MemberOf -contains $group.DistinguishedName) {
+        Write-Output "Użytkownik już jest członkiem grupy"
+    }
+    else {
+        Add-ADGroupMember -Identity $groupName -Members $userName
+        Write-Output "Użytkownik został dodany prawidłowo do grupy $groupName"
+    }
+
+    # Pobieranie aktualnej daty i godziny
+    $currentDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+    $plik = "C:\Logi\$($indexNumber)_zmiana_członkostwa_grup.txt"
+    # Sprawdzanie czy dana ścieżka istnieje, jeśli nie, utworzy ją
+    if (-not (Test-Path $plik)) {
+        $null = New-Item -ItemType File -Path $plik -Force
+    }
+
+    # Zapisywanie informacji o zmianie członkostwa grupy do pliku
+    Add-Content -Path $plik -Value "$env:USERNAME dodał użytkownika $userName do grupy $groupName o godzinie $currentDate"
+}
--- project.ps1
+++ project.ps1
@@ -257,6 +257,47 @@ function createGroup {
     $entry | Export-Csv $entryFilePath -Append -NoTypeInformation 
 }
 
+function addUserToGroup {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$groupName,
+        [Parameter(Mandatory = $true)]
+        [string]$userName
+    )
+try{
+    # Pobierz obiekt użytkownika z AD
+    $user = Get-ADUser -Identity $userName -ErrorAction SilentlyContinue
+
+    # Pobierz obiekt grupy z AD
+    $group = Get-ADGroup -Identity $groupName -ErrorAction SilentlyContinue
+}catch{
+    Write-Output "User lub grupa są nieprawidłowe. Wprowadź dane raz jeszcze."
+    return addUserToGroup
+}
+    
+
+    # Sprawdź, czy użytkownik jest członkiem grupy
+    if ($user.MemberOf -contains $group.DistinguishedName) {
+        Write-Output "Użytkownik już jest członkiem grupy"
+    }
+    else {
+        Add-ADGroupMember -Identity $groupName -Members $userName
+        Write-Output "Użytkownik został dodany prawidłowo do grupy $groupName"
+    }
+
+    # Pobieranie aktualnej daty i godziny
+    $currentDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+    $plik = "C:\Logi\$($indexNumber)_zmiana_członkostwa_grup.txt"
+    # Sprawdzanie czy dana ścieżka istnieje, jeśli nie, utworzy ją
+    if (-not (Test-Path $plik)) {
+        $null = New-Item -ItemType File -Path $plik -Force
+    }
+
+    # Zapisywanie informacji o zmianie członkostwa grupy do pliku
+    Add-Content -Path $plik -Value "$env:USERNAME dodał użytkownika $userName do grupy $groupName o godzinie $currentDate"
+}
+
 function createUserTest {
     try {
         createUser
@@ -373,6 +414,7 @@ function showGroupMenuCases {
         $selection = Read-Host "Twój wybór"
         switch ($selection) {
             '1' { createGroup }
+            '2' { addUserToGroup }
             'b' { return showMainMenu } 
             'q' { break outer }
         }
